65606ad5202f0bb42d161aa99d7f165dbcfa4e30
심은주 <nyaknya@naver.com>
2026-08-23 18:28:27 +0900

    first

---
+++ src/post/dto/create-post.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  authorId: number;
+}
