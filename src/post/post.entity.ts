e2cb45f8109135ba8df859d168176617be50e361
심은주 <nyaknya@naver.com>
2026-08-30 14:54:54 +0900

    260830 1차

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,6 +1,9 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
 
 @Entity()
+@Index('IDX_POST_TITLE', ['title']) // title에 대한 인덱스 이름 지정
+@Index('IDX_POST_AUTHOR_ID', ['authorId']) // authorId에 대한 인덱스 이름 지정
+@Index('IDX_POST_TITLE_AUTHOR_ID', ['title', 'authorId'], { unique: true }) // title과 authorId의 복합 유니크 인덱스 이름 지정
 export class Post {
   @PrimaryGeneratedColumn({
     type: 'int',
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Post } from './post.entity';
@@ -17,20 +22,44 @@ export class PostService {
   }
 
   async findOne(id: number): Promise<Post | null> {
-    return this.postRepository.findOne({ where: { id } });
+    const post = await this.postRepository.findOne({ where: { id } });
+    return post;
   }
 
   async create(createPostDto: CreatePostDto): Promise<Post> {
-    const post = this.postRepository.create(createPostDto);
-    return this.postRepository.save(post);
+    try {
+      const post = this.postRepository.create(createPostDto);
+      return await this.postRepository.save(post);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(
+          '해당 제목과 작성자를 가진 게시글이 이미 존재합니다.',
+        );
+      }
+      throw error;
+    }
   }
 
-  async update(id: number, updatePostDto: UpdatePostDto): Promise<Post | null> {
-    await this.postRepository.update(id, updatePostDto);
-    return this.postRepository.findOne({ where: { id } });
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
+    try {
+      const result = await this.postRepository.update(id, updatePostDto);
+
+      if (result.affected === 0) {
+        throw new NotFoundException(
+          `${id}에 해당하는 Post를 찾을 수 없습니다.`,
+        );
+      }
+
+      return await this.findOne(id);
+    } catch (error) {
+      throw error;
+    }
   }
 
   async remove(id: number): Promise<void> {
-    await this.postRepository.delete(id);
+    const result = await this.postRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`${id}에 해당하는 Post를 찾을 수 없습니다.`);
+    }
   }
 }
